65b74723540efe1f07e3acc1f21f0ec89ab5fd84
Angga21104050 <21104050@ittelkom-pwt.ac.id>
2025-03-17 02:21:08 +0700

    add: carousel slider

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -4,6 +4,7 @@ import '../controllers/home_controller.dart';
 import '../components/custom_app_bar.dart';
 import 'input_form_ticket.dart';
 import '../components/navigation_bar.dart' as custom;
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
@@ -15,30 +16,71 @@ class HomeView extends GetView<HomeController> {
         greeting: 'Selamat Malam',
         username: 'Airlangga Maulana Anwar',
       ),
-      body: Stack(
+      body: Column(
         children: [
-          // Background dengan gambar
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [const Color(0xFF0064D2), Colors.white, Colors.white],
-              ),
-              image: DecorationImage(
-                image: AssetImage("assets/img/map-global.png"),
-                alignment: Alignment.topCenter,
-                fit: BoxFit.contain,
-              ),
+          Expanded(
+            child: Stack(
+              children: [
+                // Background dengan gambar
+                Container(
+                  margin: EdgeInsets.only(bottom: 10),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        const Color(0xFF0064D2),
+                        Colors.white,
+                        Colors.white,
+                      ],
+                    ),
+                    image: DecorationImage(
+                      image: AssetImage("assets/img/map-global.png"),
+                      alignment: Alignment.topCenter,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                // Form di atas gambar
+                Positioned(
+                  top: -250, // Sesuaikan tinggi sesuai kebutuhan
+                  left: 0,
+                  right: 0,
+                  child: SingleChildScrollView(child: InputFormTicket()),
+                ),
+              ],
             ),
           ),
-          // Form di atas gambar
-          Positioned(
-            top: -250, // Sesuaikan tinggi sesuai kebutuhan
-            left: 0,
-            right: 0,
-            child: SingleChildScrollView(child: InputFormTicket()),
+          SizedBox(
+            height: 200, // Sesuaikan tinggi carousel
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  autoPlay: true,
+                  viewportFraction: 1.0,
+                  enlargeCenterPage: true,
+                ),
+                items:
+                    [
+                      "assets/img/iklan_1.png",
+                      "assets/img/iklan_2.png",
+                      "assets/img/contoh.jpeg",
+                    ].map((imagePath) {
+                      return Container(
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: DecorationImage(
+                            image: AssetImage(imagePath),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    }).toList(),
+              ),
+            ),
           ),
         ],
       ),
