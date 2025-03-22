b996eb0df20b06c0609272f5abd83398eda0d457
Angga21104050 <21104050@ittelkom-pwt.ac.id>
2025-03-22 17:06:11 +0700

    add: halaman pesan tiket

--- lib/app/modules/orderTicket/views/order_ticket_view.dart
+++ lib/app/modules/orderTicket/views/order_ticket_view.dart
@@ -1,23 +1,185 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/order_ticket_controller.dart';
+import '../../myTicket/components/secondary_appbar.dart';
 
 class OrderTicketView extends GetView<OrderTicketController> {
   const OrderTicketView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('OrderTicketView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'OrderTicketView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      appBar: const SecCustomAppBar(page: 'Pesan Tiket'),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xFF0064D2), Colors.white, Colors.white],
+              ),
+              image: DecorationImage(
+                image: AssetImage("assets/img/map-global.png"),
+                alignment: Alignment.topCenter,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Container(
+                height: 150,
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Colors.grey.shade300),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blueAccent.withOpacity(
+                        0.3,
+                      ), // Shadow biru soft
+                      blurRadius: 10,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    // Nama Kapal (Vertical Text)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: RotatedBox(
+                        quarterTurns: -1,
+                        child: Text(
+                          'ferryName',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF004AAD), // Biru modern
+                          ),
+                        ),
+                      ),
+                    ),
+                    // Garis Putus-putus
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Column(
+                        children: List.generate(21, (index) {
+                          return Container(
+                            width: 2,
+                            height: 3,
+                            color: Colors.grey[400], // Abu-abu soft
+                            margin: const EdgeInsets.symmetric(vertical: 2),
+                          );
+                        }),
+                      ),
+                    ),
+                    // Detail Tiket
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'class',
+                            style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF004AAD),
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '08.00',
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    '4jam',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.grey[600],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    '12.00',
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    'arrivalPort',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.grey[600],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 6),
+                          Text(
+                            'tanggal',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey[500],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 250,
+                        margin: const EdgeInsets.symmetric(
+                          vertical: 8,
+                          horizontal: 16,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(color: Colors.grey.shade300),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.blueAccent.withOpacity(
+                                0.3,
+                              ), // Shadow biru
+                              blurRadius: 10,
+                              offset: const Offset(0, 4),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
